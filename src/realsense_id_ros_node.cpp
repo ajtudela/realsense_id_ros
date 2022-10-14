ae103d648304ed2ccd71c6c4c0059e26fbf96f44
Alberto Tudela <ajtudela@gmail.com>
2022-10-14 14:45:41 +0200

    Remove sleep from main node

--- src/realsense_id_ros_node.cpp
+++ src/realsense_id_ros_node.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv){
 		while (ros::ok()){
 			realsense.update();
 			ros::spinOnce();
-			//rate.sleep(); // Remove this to relay on device frequency
+			rate.sleep(); // Remove this to relay on device frequency
 		}
 	}catch (const char* s){
 		ROS_FATAL_STREAM("[RealSense ID]: " << s);
